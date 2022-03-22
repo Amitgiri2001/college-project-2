ff90bc093cf7db1030d82c88fa3a27bafe87ad01
Amit Giri <giriamit987@gmail.com>
2022-03-22 14:13:00 +0530

    commerce arts database created

--- app.js
+++ app.js
@@ -36,7 +36,13 @@ const postArtsSchema = {
 // create one mongoose model
 const Post = mongoose.model("Post", postSchema);
 const PostArt = mongoose.model("PostArt", postArtsSchema);
+const PostSci = mongoose.model("PostSci", postArtsSchema);
+const PostCom = mongoose.model("PostCom", postArtsSchema);
+const PostGen = mongoose.model("PostGen", postArtsSchema);
 
+
+
+// --------------Create every Compose page-----------------
 app.get("/compose", function (req, res) {
     res.render("compose");
 });
@@ -59,13 +65,12 @@ app.post("/compose", function (req, res) {
 
 })
 
-
-// compose Arts Section------
+// compose Arts Section
 app.get("/composeArts", function (req, res) {
     res.render("composeArts");
 });
 app.post("/composeArts", function (req, res) {
-// create one new post with small 'p'--> and it access all the compose form
+
     const postArt = new PostArt({
         postTitle: req.body.postTitle,
         postDetails: req.body.postDetails,
@@ -74,15 +79,81 @@ app.post("/composeArts", function (req, res) {
         postScope: req.body.postScope,
         postJob: req.body.postJob
     });
-    // then just simply save the post without any error .
-    // and redirect to home page for showing
-    postArt.save(function (err) {
+        postArt.save(function (err) {
         if (!err) {
             res.redirect("/arts");
         }
     });
 
 })
+
+//create compose science Section-----
+app.get("/composeSci", function (req, res) {
+    res.render("composeSci");
+});
+app.post("/composeSci", function (req, res) {
+
+    const postSci = new PostSci({
+        postTitle: req.body.postTitle,
+        postDetails: req.body.postDetails,
+        postEligibility: req.body.postEligibility,
+        postAdmission: req.body.postAdmission,
+        postScope: req.body.postScope,
+        postJob: req.body.postJob
+    });
+    postSci.save(function (err) {
+        if (!err) {
+            res.redirect("/science");
+        }
+    });
+
+})
+//create compose commerce Section-----
+app.get("/composeCom", function (req, res) {
+    res.render("composeCom");
+});
+app.post("/composeCom", function (req, res) {
+
+    const postCom = new PostCom({
+        postTitle: req.body.postTitle,
+        postDetails: req.body.postDetails,
+        postEligibility: req.body.postEligibility,
+        postAdmission: req.body.postAdmission,
+        postScope: req.body.postScope,
+        postJob: req.body.postJob
+    });
+    postCom.save(function (err) {
+        if (!err) {
+            res.redirect("/commerce");
+        }
+    });
+
+})
+//create compose General Section-----
+app.get("/composeGen", function (req, res) {
+    res.render("composeGen");
+});
+app.post("/composeGen", function (req, res) {
+
+    const postGen = new PostGen({
+        postTitle: req.body.postTitle,
+        postDetails: req.body.postDetails,
+        postEligibility: req.body.postEligibility,
+        postAdmission: req.body.postAdmission,
+        postScope: req.body.postScope,
+        postJob: req.body.postJob
+    });
+    postGen.save(function (err) {
+        if (!err) {
+            res.redirect("/general");
+        }
+    });
+
+})
+
+
+
+// -----------------render Every Stream Page-----------------
 // In home page we must try to find all the posts and then render home page
 app.get("/", function (req, res) {
     Post.find({}, function (err, posts) {
@@ -91,6 +162,7 @@ app.get("/", function (req, res) {
         });
     });
 });
+// Render arts page
 app.get("/arts", function (req, res) {
     PostArt.find({}, function (err, postArts) {
         res.render("arts", {
@@ -98,6 +170,32 @@ app.get("/arts", function (req, res) {
         });
     });
 });
+// Render Science page
+app.get("/science", function (req, res) {
+    PostSci.find({}, function (err, postScis) {
+        res.render("science", {
+            postScis: postScis
+        });
+    });
+});
+app.get("/commerce", function (req, res) {
+    PostCom.find({}, function (err, postComs) {
+        res.render("commerce", {
+            postComs: postComs
+        });
+    });
+});
+app.get("/general", function (req, res) {
+    PostGen.find({}, function (err, postGens) {
+        res.render("general", {
+            postGens: postGens
+        });
+    });
+});
+
+
+
+//----------------- Code for getting every post as a single Page-----------------
 app.get("/posts/:postId", function (req, res) {
     // we use id for access all new post in single page
     const requestedPostId = req.params.postId;
@@ -129,6 +227,58 @@ app.get("/postArts/:postId", function (req, res) {
     });
 
 });
+// for postScience render evert post single page----
+app.get("/postScis/:postId", function (req, res) {
+    // we use id for access all new post in single page
+    const reqPostId = req.params.postId;
+    PostSci.findOne({ _id: reqPostId }, function (err, post) {
+        res.render("postScis", {
+            postTitle: post.postTitle,
+            postDetails: post.postDetails,
+            postEligibility: post.postEligibility,
+            postAdmission: post.postAdmission,
+            postScope: post.postScope,
+            postJob: post.postJob
+        });
+    });
+
+});
+// for postCommerce render evert post single page----
+app.get("/postComs/:postId", function (req, res) {
+    // we use id for access all new post in single page
+    const reqPoId = req.params.postId;
+    PostCom.findOne({ _id: reqPoId }, function (err, post) {
+        res.render("postComs", {
+            postTitle: post.postTitle,
+            postDetails: post.postDetails,
+            postEligibility: post.postEligibility,
+            postAdmission: post.postAdmission,
+            postScope: post.postScope,
+            postJob: post.postJob
+        });
+    });
+
+});
+app.get("/postGens/:postId", function (req, res) {
+    // we use id for access all new post in single page
+    const reqId = req.params.postId;
+    PostGen.findOne({ _id: reqId }, function (err, post) {
+        res.render("postGens", {
+            postTitle: post.postTitle,
+            postDetails: post.postDetails,
+            postEligibility: post.postEligibility,
+            postAdmission: post.postAdmission,
+            postScope: post.postScope,
+            postJob: post.postJob
+        });
+    });
+
+});
+
+
+
+
+// port setting---------------------
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
